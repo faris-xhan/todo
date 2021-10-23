c06680009b3eca26e4a94ab2abc30bd3da6f02bf
faris <faris@mail.com>
2021-10-23 10:09:42 -0700

    implement: post method on login  :triangular_flag_on_post:

--- routes/login.js
+++ routes/login.js
@@ -5,4 +5,22 @@ router.get("/", (req, res, next) => {
    res.render("login", { title: "TODO - Login" });
 });
 
+router.post("/", (req, res, next) => {
+   const { uname, passwd } = req.body;
+
+   if (!uname || !passwd) {
+      return res.render("login", {
+         uname: uname,
+         passwd: passwd,
+         message: "Username or password is incorrect.",
+      });
+   }
+
+   return res.render("login", {
+      uname: uname,
+      passwd: passwd,
+      message: "User logged in succesfully",
+   });
+});
+
 module.exports = router;
