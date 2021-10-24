caae183e67e4804d16e733554f218d6b2f225dbd
faris <faris@mail.com>
2021-10-24 00:40:02 -0700

    implemented: add createTodo service :hammer:

--- services/todosListService.js
+++ services/todosListService.js
@@ -7,6 +7,15 @@ const getTodos = async (user_id) => {
    return rows;
 };
 
+const createTodo = async (owner_id, task) => {
+   const result = await db.query(
+      `INSERT INTO todosList(onwer_id, task) VLAUES(?, ?)`[(owner_id, task)]
+   );
+
+   return result;
+};
+
 module.exports = {
    getTodos,
+   createTodo,
 };
